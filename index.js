6306fc8365d689d18a224133a84fe96fa2d82619
mati olmos <matiasolmosestudiorocha@gmail.com>
2022-12-22 20:19:28 -0300

    refactorizacion terminada

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 
 const mongoose = require('mongoose');
 const app = require('./app');
-const port = process.env.PORT || 3001;
+const config = require('./config');
 
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/shop', (err, res) => {
+mongoose.connect(config.db, (err, res) => {
     if (err) {
         return console.log(`error al conectar a la base de datos: ${err}`)
     }
     console.log("conección a la base de datos establecida")
 
-    app.listen(port, () => {
-        console.log(`API REST corriendo en http://localhost:${port}`);
+    app.listen(config.port, () => {
+        console.log(`API REST corriendo en http://localhost:${config.port}`);
     })
 });
